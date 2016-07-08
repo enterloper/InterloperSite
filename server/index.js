bc2d2f007dd8e71a8d687c8d497fa82fe32d756b
Richard Boothe <richardjboothe@me.com>
2016-07-08 11:12:18 -0400

    more endpoints for db manipulation

--- server/index.js
+++ server/index.js
@@ -51,29 +51,29 @@ app.get('/portfolio', function(req, res) {
 
 /***************** BLOG ENDPOINTS *****************/
 
-app.get('/posts', function(req, res) {
+app.get('/posts', function(req, res, next) {
   Posts.getAll()
   .then(function(data) {
     console.log(data);
     res.send(data);
-  });
+  }).catch(next);
 });
 
 
-app.get('/posts/id/:postID', function(req, res){
+app.get('/posts/id/:postID', function(req, res, next){
   Posts.getPostByID(req.params.postID)
   .then(function(data){
     console.log(data);
     res.send(data);
-  });
+  }).catch(next);
 });
 
-app.get('/posts/category/:category', function(req, res) {
+app.get('/posts/category/:category', function(req, res, next) {
   Posts.getPostByCategory(req.params.category)
   .then(function(data) {
     console.log(data);
     res.send(data);
-  });
+  }).catch(next);
 });
 
 var newDummyBlog = {
@@ -83,36 +83,51 @@ var newDummyBlog = {
   "blog_body":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   "toy_problem_attached": false
 };
-app.post('/posts/add', function(req, res){
-  Posts.addNewBlogPost(newDummyBlog).then(function(data) {
-    console.log(data);
-    res.send(data);
-  });
+app.post('/posts/add', function(req, res, next){
+  Posts.addNewBlogPost(newDummyBlog)
+  .then(function(resp) {
+    console.log(resp);
+    res.send(resp);
+  }).catch(next);
+});
+
+var modBlog = {
+  "blog_id": 4,
+  "blog_body":"blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah"
+};
+
+app.put('/posts/put', function(req,res, next){
+  console.log('WE IN PUT')
+  Posts.editBlogPost(modBlog)
+  .then(function(resp) {
+    console.log(resp);
+    res.send(resp);
+  }).catch(next);
 });
 /************* TOY PROBLEM ENDPOINTS *************/
 
-app.get('/problems', function(req, res) {
+app.get('/problems', function(req, res, next) {
   ToyProbs.getAll()
   .then(function(resp) {
     console.log(resp);
     res.send(resp);
-  });
+  }).catch(next);
 });
 
-app.get('/problems/:toyProbID', function(req, res){
+app.get('/problems/:toyProbID', function(req, res, next){
   ToyProbs.getToyProbByID(req.params.toyProbID)
   .then(function(data){
     console.log(data);
     res.send(data);
-  });
+  }).catch(next);
 });
 
-app.get('/problems/difficulty/:level', function(req, res) {
+app.get('/problems/difficulty/:level', function(req, res, next) {
   ToyProbs.getToyProbByDifficulty(req.params.level)
   .then(function(data) {
     console.log(data);
     res.send(data);
-  });
+  }).catch(next);
 });
 
 /************* PORTFOLIO ENDPOINTS *************/
--- server/posts/posts_model.js
+++ server/posts/posts_model.js
@@ -8,48 +8,47 @@ var Posts = module.exports;
   };
 
   Posts.getPostByID = function(postID) {
-    return db('blogs').where({
+    return db('blogs')
+    .where({
       'blog_id' : postID
     });
   };
 
   Posts.getPostByTitle = function(postTitle) {
-    return db('blogs').where({
+    return db('blogs')
+    .where({
       'blog_title' : postTitle
     });
   };
 
   Posts.getPostByCategory = function(postCategory) {
-    return db('blogs').where({
+    return db('blogs')
+    .where({
       'blog_category' : postCategory
     });
   };
 
   Posts.getByDescription = function(keyword) {
-    return db('blogs').where({})
+    return db('blogs')
+    .where({});
   };
 
   Posts.addNewBlogPost = function(data) {
-    return db('blogs').insert(data);
+    return db('blogs')
+    .insert(data);
   };
 
-
+  Posts.editBlogPost = function() {
+    console.log();
+    return db('blogs')
+    .where('blog_id', '=', 4)
+    .update({
+      blog_body : "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah"
+    });
+  };
 /************* BLOG ENDPOINTS *************/
 // Get bound toyproblems if present
 // create mods/push/put
-
-
-// update.update(data, [returning]) / .update(key, value, [returning]) 
-// Creates an update query, taking a hash of properties or a key/value pair to be updated based on the other query constraints. Resolves the promise / fulfills the callback with the number of affected rows for the query. If a key to be updated has value undefined it is ignored.
-
-// knex('books')
-//   .where('published_date', '<', 2000)
-//   .update({
-//     status: 'archived',
-//     thisKeyIsSkipped: undefined
-//   })
-// // Returns [1] in "mysql", "sqlite", "oracle"; [] in "postgresql" unless the 'returning' parameter is set.
-// knex('books').update('title', 'Slaughterhouse Five')
 
 // del / delete.del() 
 // Aliased to del as delete is a reserved word in javascript, this method deletes one or more rows, based on other conditions specified in the query. Resolves the promise / fulfills the callback with the number of affected rows for the query.
