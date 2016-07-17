e038e42f1acfed213c19c02f0243cb3abca5606d
Richard Boothe <richardjboothe@me.com>
2016-07-17 00:24:31 -0400

    streamline routing

--- js/build/production.js
+++ js/build/production.js
@@ -38,17 +38,14 @@ var rootPath = path.normalize(__dirname + './../public');
 
 // Set up Handlebars engine
 app.engine('handlebars', exphbs({defaultLayout: 'main',
-                                 partialsDir: [
-                                  "views/partials/" 
-                                 ]
+                                 partialsDir: "views/partials/",
+                                 layoutsDir:  "views/layouts/"
                                }));
 app.set('view engine', 'handlebars');
 app.enable('view cache');
 
 //serve static files in public directory, without processing them.
 app.use(express.static('public'));
-console.log("(((({{{ROOTPATH}}})))))):", rootPath);
-app.use("/pages", express.static(rootPath + '/pages'));
 app.use("/style", express.static(rootPath + '/style'));
 app.use("/img", express.static(rootPath + '/img'));
 app.use("/lib", express.static(rootPath + '/lib'));
@@ -207,64 +204,63 @@ app.param('id', function(req, res, next, id) {
 });
 
 //GET all posts
-app.get('/api/posts', function(req, res, next) {
-  Posts.getAll()
-  .then(function(data) {
-    res.status(200).json(data);
-  }).catch(next);
-});
-
+app.route('/api/posts') 
+  .get(function(req, res, next) {
+    Posts.getAll()
+    .then(function(data) {
+      res.status(200).json(data);
+    }).catch(next);
+  })
 //Add a post
-app.post('/api/posts/', function(req, res, next) {
-  Posts.addNewBlogPost(req.body)
-  .then(function(resp) {
-    res.status(201).json(res.req.body);
-  }).catch(function(err) {
-    console.error(err.stack);
-    next();
+  .post(function(req, res, next) {
+    Posts.addNewBlogPost(req.body)
+    .then(function(resp) {
+      res.status(201).json(res.req.body);
+    }).catch(function(err) {
+      console.error(err.stack);
+      next();
+    });
   });
-});
 
 
 //GET post by ID
-app.get('/api/posts/:id', function(req, res, next) {
-  Posts.getPostByID(req.params.id)
-  .then(function(data){
-    res.status(200).json(data);
-  }).catch(function(err) {
-    console.error(err.stack);
-    next();
-  });
-});
-
+app.route('/api/posts/:id')
+  .get(function(req, res, next) {
+    Posts.getPostByID(req.params.id)
+    .then(function(data){
+      res.status(200).json(data);
+    }).catch(function(err) {
+      console.error(err.stack);
+      next();
+    });
+  })
 //Edit a post
-app.put('/api/posts/:id', function(req, res, next){
-  Posts.editBlogPost(req.params.id, req.body)
-  .then(function(resp) {
-    console.log("Modified on blog number "+req.params.id+":", res.req.body);
-    res.status(200).json(res.req.body);
+  .put(function(req, res, next){
+    Posts.editBlogPost(req.params.id, req.body)
+    .then(function(resp) {
+      console.log("Modified on blog number "+req.params.id+":", res.req.body);
+      res.status(200).json(res.req.body);
+    })
+    .catch(function(err){
+      console.error(err.stack);
+      next();
+    });
   })
-  .catch(function(err){
-    console.error(err.stack);
-    next();
-  });
-});
-
 //Delete a post
-app.delete('/api/posts/:id', function(req, res, next) {
-  Posts.deletePost(req.params.id)
-  .then(function(resp) {
-    console.log("Deleted blog number "+req.params.id+":", res.req.body);
-    res.status(200).json(resp);
-  })
-  .catch(function(err){
-    console.error(err.stack);
-    next();
+  .delete(function(req, res, next) {
+    Posts.deletePost(req.params.id)
+    .then(function(resp) {
+      console.log("Deleted blog number "+req.params.id+":", res.req.body);
+      res.status(200).json(resp);
+    })
+    .catch(function(err){
+      console.error(err.stack);
+      next();
+    });
   });
-});
 
 //GET post by Title
-app.get('/api/posts/title:title', function(req, res, next){
+app.get('/api/posts/title/:title', function(req, res, next){
   Posts.getPostByTitle(req.params.title)
   .then(function(data){
     res.status(200).json(data);
@@ -288,66 +284,64 @@ app.get('/api/posts/category/:category', function(req, res, next) {
 /************* TOY PROBLEM ENDPOINTS *************/
 
 //GET ALL toy problems
-app.get('/api/problems', function(req, res, next) {
-  ToyProbs.getAll()
-  .then(function(resp) {
-    res.send(resp);
-  }).catch(function(err){
-    console.error(err.stack);
-    next();
+app.route('/api/problems') 
+  .get(function(req, res, next) {
+    ToyProbs.getAll()
+    .then(function(resp) {
+      res.send(resp);
+    }).catch(function(err){
+      console.error(err.stack);
+      next();
+    });
+  })
+  //Add a toy problems
+  .post(function(req, res, next) {
+    ToyProbs.addNewToyProblem(req.body)
+    .then(function(resp) {
+      res.status(201).json(res.req.body);
+    }).catch(function(err) {
+      console.error(err.stack);
+      next();
+    });
   });
-});
-
-//Add a toy problems
-app.post('/api/problems', function(req, res, next) {
-  ToyProbs.addNewToyProblem(req.body)
-  .then(function(resp) {
-    res.status(201).json(res.req.body);
-  }).catch(function(err) {
-    console.error(err.stack);
-    next();
-  });
-});
 
 //GET a toy problem by ID
-app.get('/api/problems/:id', function(req, res, next){
-  ToyProbs.getToyProbByID(req.params.id)
-  .then(function(data){
-    res.send(data);
-  }).catch(function(err){
-    console.error(err.stack);
-    next();
-  });
-});
-
-
-//Edit a Toy Problem
-app.put('/api/problems/:id', function(req, res, next) {
-  ToyProbs.editToyProblem(req.params.id, req.body)
-  .then(function(resp) {
-    console.log("Modified on toy problem number "+req.params.id+":", res.req.body);
-    res.status(200).json(res.req.body);
+app.route('/api/problems/:id') 
+  .get(function(req, res, next){
+    ToyProbs.getToyProbByID(req.params.id)
+    .then(function(data){
+      res.send(data);
+    }).catch(function(err){
+      console.error(err.stack);
+      next();
+    });
   })
-  .catch(function(err){
-    console.error(err.stack);
-  });
-});
-
-//Delete a post
-app.delete('/api/problems/:id', function(req, res, next) {
-  ToyProbs.deleteToyProblem(req.params.id)
-  .then(function(resp) {
-    console.log("Deleted toy problem number "+req.params.id+":", res.req.body);
-    res.status(200).json(res.body);
+  //Edit a Toy Problem
+  .put(function(req, res, next) {
+    ToyProbs.editToyProblem(req.params.id, req.body)
+    .then(function(resp) {
+      console.log("Modified on toy problem number "+req.params.id+":", res.req.body);
+      res.status(200).json(res.req.body);
+    })
+    .catch(function(err){
+      console.error(err.stack);
+    });
   })
-  .catch(function(err){
-    console.error(err.stack);
+  //Delete a post
+  .delete(function(req, res, next) {
+    ToyProbs.deleteToyProblem(req.params.id)
+    .then(function(resp) {
+      console.log("Deleted toy problem number "+req.params.id+":", res.req.body);
+      res.status(200).json(res.body);
+    })
+    .catch(function(err){
+      console.error(err.stack);
+    });
   });
-});
 
 
 //GET toy problem by Title
-app.get('/api/problems/title:title', function(req, res, next){
+app.get('/api/problems/title/:title', function(req, res, next){
   ToyProbs.getToyProbByTitle(req.params.title)
   .then(function(data){
     res.status(200).json(data);
@@ -395,42 +389,41 @@ app.post('/api/projects/', function(req, res, next) {
 
 
 //GET project by ID
-app.get('/api/projects/:id', function(req, res, next){
-  Projects.getProjectByID(req.params.id)
-  .then(function(data){
-    res.status(200).json(data);
-  }).catch(function(err) {
-    console.error(err.stack);
-    next();
-  });
-});
-
-//Edit a project
-app.put('/api/projects/:id', function(req, res, next){
-  Projects.editProject(req.params.id, req.body)
-  .then(function(resp) {
-    console.log("Modified on project number "+req.params.id+":", res.req.body);
-    res.status(200).json(res.req.body);
+app.route('/api/projects/:id')
+  .get(function(req, res, next){
+    Projects.getProjectByID(req.params.id)
+    .then(function(data){
+      res.status(200).json(data);
+    }).catch(function(err) {
+      console.error(err.stack);
+      next();
+    });
   })
-  .catch(function(err){
-    console.error(err.stack);
-  });
-});
-
-//Delete a project
-app.delete('/api/projects/:id', function(req, res, next) {
-  Projects.deleteProject(req.params.id)
-  .then(function(resp) {
-    console.log("Deleted project number "+req.params.id+":", res.req.body);
-    res.status(200).json(resp);
+  //Edit a project
+  .put(function(req, res, next){
+    Projects.editProject(req.params.id, req.body)
+    .then(function(resp) {
+      console.log("Modified on project number "+req.params.id+":", res.req.body);
+      res.status(200).json(res.req.body);
+    })
+    .catch(function(err){
+      console.error(err.stack);
+    });
   })
-  .catch(function(err){
-    console.error(err.stack);
+  //Delete a project
+  .delete(function(req, res, next) {
+    Projects.deleteProject(req.params.id)
+    .then(function(resp) {
+      console.log("Deleted project number "+req.params.id+":", res.req.body);
+      res.status(200).json(resp);
+    })
+    .catch(function(err){
+      console.error(err.stack);
+    });
   });
-});
 
 //GET project by Title
-app.get('/api/projects/title:title', function(req, res, next){
+app.get('/api/projects/title/:title', function(req, res, next){
   Projects.getProjectByTitle(req.params.title)
   .then(function(data){
     res.status(200).json(data);
