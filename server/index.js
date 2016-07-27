85ff172aed2b0c020b1f8feca133f5e708cbac97
Richard Boothe <richardjboothe@me.com>
2016-07-27 15:01:20 -0400

    bug fix attempt number 35581095

--- server/index.js
+++ server/index.js
@@ -33,35 +33,18 @@ var hbs = exphbs.create({
   layoutsDir: 'server/views/layouts/'
 });
 
-// Register `hbs` as our view engine using its bound `engine()` function.
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.set('view cache', true);
-app.set('case sensitive routing', false); 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.set( 'port', (process.env.PORT || 3000) );
 
+//middleware
 //assetFolder for path to public directory => Interloper/public
 var assetFolder = path.resolve(__dirname, './../../public');
+//serve static files in public directory, without processing them.
+app.use( express.static('public') );
 app.use( express.static(assetFolder) );
 app.use('/img', express.static(assetFolder + '/img') );
 app.use( express.static(assetFolder + '/style') );
 app.use( express.static(assetFolder + '/src') );
-
-//serve static files in public directory, without processing them.
-app.use(express.static('public'));
-//middleware
 // app.use(morgan('dev'));
-
-app.set('views', __dirname + '/views');
-
-
-//DISABLE RETURNING SERVER INFORMATION VIA Express' default X-Powered-By
-app.disable('x-powered-by');
-app.param('id', function(req, res, next, id) {
-  req.params.id = Number(id);
-  next();
-});
 
 //ROUTERS
 app.use("/api", APIRouter);
@@ -73,19 +56,36 @@ app.use(function(req, res) {
   res.status(404).render('404');
 });
 
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('handlebars', hbs.engine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.set('view cache', true);
+app.set('case sensitive routing', false); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//DISABLE RETURNING SERVER INFORMATION VIA Express' default X-Powered-By
+app.disable('x-powered-by');
+app.param('id', function(req, res, next, id) {
+  req.params.id = Number(id);
+  next();
+});
+
 //ERROR HANDLING FOR RESPONSE CODES OTHER THAN 200
 app.get('/error', function(err, req, res, next) {
   //set status to 500 and render error page
   // console.error(err.message);
   res.status(500).render('500');
 });
+
 app.use(function(err, req, res, next) {
     // console.error(err.stack);
     res.status(500).render('500');
 });
 
-app.listen(config.port || 3000, function(){
-  // console.log('Listening on port:' , config.port);
+app.listen(app.get('port'), function(){
+  console.log('Node app is running on port:' , app.get('port'));
 });
 
 module.exports = app;  
