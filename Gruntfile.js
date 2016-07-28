e266878441115ff403ff111ed8064068d627e460
Richard Boothe <richardjboothe@me.com>
2016-07-28 10:40:21 -0400

    modify grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,44 +49,17 @@ module.exports = function(grunt) {
                     dest: 'public/img/'
                 }]
             }
-        },
-        watch: {
-            scripts: {
-                files: [ 
-                'public/lib/*.js', // All JS in the libs folder
-                'public/src/*.js', 
-                'server/*.js',
-                'server/config/*.js',
-                '/server/posts/*.js',
-                '/server/projects/*.js',
-                '/server/toy_problems/*.js',
-                '/server/routes/*.js',
-                'server/seeds/*.js',
-                'server/migrations/*.js',
-                '/server/views/*.js',
-                '/server/views/layouts*.js',
-                '/server/views/partials*.js',
-                '/server/views/shared*.js'
-                ],
-                tasks: ['concat', 'uglify'],
-                options: {
-                    spawn: false,
-                    watch: true
-                },
-            } 
         }
-
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     ['grunt-contrib-concat',
     'grunt-contrib-uglify',
     'grunt-contrib-imagemin',
-    'grunt-contrib-watch',
     "grunt-contrib-cssmin",
     "grunt-contrib-handlebars"].forEach(function(task) { grunt.loadNpmTasks(task);});
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'watch', 'cssmin', 'handlebars']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'cssmin', 'handlebars']);
 
 };
