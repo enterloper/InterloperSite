dabdaa829311f647b443955941a2b443652d0616
Richard Boothe <richardjboothe@me.com>
2016-07-29 11:06:23 -0400

    fix bug

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -7,7 +7,7 @@ console.log('MAIN DIRNAME:', __dirname);
 var assetFolder = Path.resolve(__dirname, './../../public');
 console.log('MAIN ASSETFOLER:',assetFolder);
 MainRouter.use( express.static(assetFolder) );
-app.use('/img', express.static(__dirname + './../../public/img'));
+MainRouter.use('/img', express.static(__dirname + './../../public/img'));
 /***************** HOME PAGE ROUTING *****************/
 MainRouter.get('/', function(req, res){
   res.render('home');
